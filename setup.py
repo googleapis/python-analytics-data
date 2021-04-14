647306e23039f3b35d27e0a1124a7cc5b5860776
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-04-14 19:31:57 -0400

    chore: prevent normalization of semver versioning

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with io.open(readme_filename, encoding="utf-8") as readme_file:
 
 setuptools.setup(
     name="google-analytics-data",
-    version=version,
+    version=setuptools.sic(version),
     long_description=readme,
     author="Google LLC",
     author_email="googleapis-packages@google.com",
