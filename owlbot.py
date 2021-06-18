6c41ee69c6e3e3d2adc25f01da46ba377583516e
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-06-18 05:55:33 -0400

    chore: update Owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -41,7 +41,10 @@ s.remove_staging_dirs()
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(cov_level=99, microgenerator=True)
 s.move(
-    templated_files, excludes=[".coveragerc"]
+    templated_files, excludes=[
+        ".coveragerc",
+        ".kokoro/docs/*"
+    ]
 )  # the microgenerator has a good coveragerc file
 
 # fix coverage target
