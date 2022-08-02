be7d49ba2715271c409a753ad5d664c2eac532b1
Anthonios Partheniou <partheniou@google.com>
2022-08-02 10:19:42 -0400

    revert

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
     install_requires=(
         "google-api-core[grpc] >= 1.32.0, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.*,!=2.4.*,!=2.5.*,!=2.6.*,!=2.7.*",
         "proto-plus >= 1.15.0, <2.0.0dev",
-        "protobuf >=4.21.4, <4.22.0",
+        "protobuf >= 3.19.0, <4.0.0dev",
     ),
     python_requires=">=3.7",
     classifiers=[
